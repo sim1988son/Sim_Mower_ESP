e8264746f0effe8000d3353aba58ccdd5a781c4c
SimpSon <sim1988son@hotmail.com>
2023-09-27 22:24:46 +0200

    up

---
+++ src/config.h
@@ -0,0 +1,9 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+const int MA1 = 55;
+const int MA2 = 66;
+const int MB1 = 77;
+const int MB2 = 88;
+
+#endif
--- src/main.ino
+++ src/main.ino
@@ -1,7 +1,11 @@
-#include <arduino.h>
+#include <Arduino.h>
 #include "config.h"
+#include "mmotor.h"
+
+Motor.motor(MA1, MA2, MB1, MB2);
 
 Void setup(){
+Motor.begin();
 
 }
 
