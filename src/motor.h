144af3dfe2576ada8db3143ad3d803a0b61538a2
SimpSon <sim1988son@hotmail.com>
2023-09-28 19:17:50 +0200

    Update motor.h
    up

--- src/motor.h
+++ src/motor.h
@@ -5,33 +5,14 @@
 
 class Motor
 {
-private:
-int _pin_A1;
-int _pin_A2;
-int _pin_B1;
-int _pin_B2;
+  int _pin_A1;
+  int _pin_A2;
+  int _pin_B1;
+  int _pin_B2;
 public:
-  motor(int pin_A1, int pin_A2, int pin_B1, int pin_B2){
-    _pin_A1 = pin_A1;
-    _pin_A2 = pin_A2;
-    _pin_B1 = pin_B1;
-    _pin_B2 = pin_B2;
-  }
-  ~motor();
-  void begin(){
-    ledcSetup(LED_CHANELL_1A, FREQ, RESOLUTION);
-    ledcSetup(LED_CHANELL_2A, FREQ, RESOLUTION);
-    ledcSetup(LED_CHANELL_1B, FREQ, RESOLUTION);
-    ledcSetup(LED_CHANELL_2B, FREQ, RESOLUTION);
-    ledcAttachPin(_pin_A1, LED_CHANELL_1A); 
-    ledcAttachPin(_pin_A2, LED_CHANELL_2A);
-    ledcAttachPin(_pin_B1, LED_CHANELL_1B); 
-    ledcAttachPin(_pin_B2, LED_CHANELL_2B);
-    ledcWrite(LED_CHANELL_1A, 0);
-    ledcWrite(LED_CHANELL_2A, 0);
-    ledcWrite(LED_CHANELL_1B, 0);
-    ledcWrite(LED_CHANELL_2B, 0);
-  }
+  Motor(int, int, int, int);
+  ~Motor();
+  void begin();
 
 }
 
